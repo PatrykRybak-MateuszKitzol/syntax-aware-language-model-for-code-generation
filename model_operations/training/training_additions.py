6a92aaad0b495f6a3eb8f58c387f770999e9ba2a
patryk-rybak <patrykrybak6@gmail.com>
2025-05-11 09:52:45 +0200

    Corrected logitsprocessor mode detection

--- model_operations/training/training_additions.py
+++ model_operations/training/training_additions.py
@@ -134,19 +134,20 @@ class SemanticCodeLogitsMask(LogitsProcessor):
         """
         Analizing this code it is worth remebering that "semantic start" token belongs to code tokens and "semantic stop" token belongs to semantic ones.
         """
+        torch.set_printoptions(threshold=10000)
         batch_size, cur_len = input_ids.shape
         for b in range(batch_size):
             latest_token_id = input_ids[b][-1].item()
 
-            is_semantic = None
             # Determine the context based on the latest token
             if latest_token_id in self.code_token_ids:
+                is_semantic = False
                 if latest_token_id == self.semantic_start_id:
                     is_semantic = True
-                else:
+            else:
+                is_semantic = True
+                if latest_token_id == self.semantic_stop_id:
                     is_semantic = False
-            elif latest_token_id == self.semantic_stop_id:
-                is_semantic = False
                 
             # Prepare the mask to exclude different different context tokens 
             if is_semantic:
@@ -157,6 +158,6 @@ class SemanticCodeLogitsMask(LogitsProcessor):
             scores[b] = torch.where(mask, scores[b], torch.tensor(float("-inf"), device=scores.device))
             scores[b][0] = -inf
 
-            # Condition the end of the sequence
+            # Condition the end of the sequence (T5 original eos)
             # if cur_len < 10: scores[b][1] = -inf
         return scores
